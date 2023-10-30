027d4b4e07e52a6cda4a9906668f0ad0f12b765f
eliodn <lolabourgeot2@gmail.com>
2023-10-30 06:38:02 -0400

    update currentLocation.js

--- src/CurrentLocation.js
+++ src/CurrentLocation.js
@@ -1,23 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
-export default function CurrentLocation (props) {
-  staticprops = { refresh: props.func.isRequired };
-
-  _click(event) {
+export default function CurrentLocation(props) {
+  const _click = () => {
     navigator.geolocation.getCurrentPosition((position) => {
       props.refresh(position.coords.latitude, position.coords.longitude);
     });
-  }
+  };
 
-   {
-    return (
-      <button
-        className="float-left btn btn-success"
-        onClick={(event) =>_click(event)}
-      >
-        Current
-      </button>
-    );
-  }
+  return (
+    <button className="float-left btn btn-success" onClick={_click}>
+      Current
+    </button>
+  );
 }
+
+CurrentLocation.propTypes = {
+  refresh: PropTypes.func.isRequired,
+};
